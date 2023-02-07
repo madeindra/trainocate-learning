2f54a53d0218dee90c239c1663b6bd5c1895fe2c
Made Indra <made.indra@protonmail.com>
2023-02-07 16:57:57 +0700

    add new func implementation

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 )
 
 type List interface {
-	Sum(number []int64) int64
-	Average(number []float64) float64
+	Sum() int64
+	Average() float64
+	IsTooLong(list interface{}) (bool, error)
 }
 
 type Numbers struct {
@@ -24,10 +25,22 @@ func (n Numbers) Average() float64 {
 	return mathLib.Average(n.floatList)
 }
 
+func (n Numbers) IsTooLong(list interface{}) (bool, error) {
+	return mathLib.IsTooLong(list)
+}
+
 func main() {
 	num := Numbers{
 		intList:   []int64{1, 2, 3, 4, 5},
 		floatList: []float64{1, 2, 3, 4, 5},
+	}
+
+	if tooLong, err := num.IsTooLong(num.intList); tooLong || err != nil {
+		panic("sum operation not possible")
+	}
+
+	if tooLong, err := num.IsTooLong(num.floatList); tooLong || err != nil {
+		panic("average operation not possible")
 	}
 
 	sum := num.Sum()
